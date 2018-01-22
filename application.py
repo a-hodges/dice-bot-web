f1ddefb231b60c9e7cdd6ca560f2ed5badf7a0bf
b-hodges <brjhodges@gmail.com>
2018-01-22 13:43:58 -0600

    Lint and style

--- application.py
+++ application.py
@@ -6,7 +6,6 @@ sslify = SSLify(application)
 application.config['PREFERRED_URL_SCHEME'] = 'https'
 
 if __name__ == '__main__':
-    import os
     application.run(
         host='127.0.0.1',
         port=5000,

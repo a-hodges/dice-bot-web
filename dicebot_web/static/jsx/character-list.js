3ae6a3c2d869c3ca5cd937d045bd2cc19b8fd2e3
b-hodges <16946799+b-hodges@users.noreply.github.com>
2018-02-08 17:42:13 -0600

    Changed DM to DM character

--- dicebot_web/static/jsx/character-list.js
+++ dicebot_web/static/jsx/character-list.js
@@ -32,7 +32,7 @@ class Character extends React.Component {
             character = <span>{this.props.character.name}</span>
         }
         else if (this.props.character.user === "DM") {
-            character = <span>{this.props.character.name}: DM</span>
+            character = <span>{this.props.character.name}: DM character</span>
         }
         else if (this.state.user === undefined) {
             return <li className="list-group-item list-group-item-warning">Loading user...</li>
